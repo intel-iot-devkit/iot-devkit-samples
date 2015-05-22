e344bfdc27248ee1a4b46c80cecccac2b07e05e7
sisinty sasmita patra <sisinty.s.patra@intel.com>
2015-05-21 17:38:46 -0700

    starter: added two new files starterSample and Readme
    signed-off-by: Sisinty Sasmita Patra <sisinty.s.patra@intel.com>
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

---
+++ sensor-kits-samples/StarterSample/starterSample.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author: Sisinty Sasmita Patra <sisinty.s.patra@intel.com>
+ * Copyright (c) 2015 Intel Corporation.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *   
+ */
+
+#include <iostream>
+#include "jhd1313m1.h"   //lcd display
+#include "grove.h"     
+#include "adxl345.h"     //3-Axis digital accelerometer
+#include "buzzer.h"	 //buzzer
+#include "mic.h"         //sound sensor
+#include "at42qt1070.h"  //Touch sensor
+#include "ldt0028.h"     //Piezo vibration sensor 
+
+using namespace std;
+using namespace upm;
+
+int main()
+{
+}
