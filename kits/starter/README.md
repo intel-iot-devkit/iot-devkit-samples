977096f3da09b96050473c96c1d7aad1643132aa
Mihai Tudor Panu <mihai.tudor.panu@intel.com>
2015-05-21 17:57:34 -0700

    starter: fixed sensor include and renamed folders
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

--- kits/starter/README.md
+++ kits/starter/README.md
@@ -1,8 +1,10 @@
+Starter Kit Sample
+==============
+
 Author: Sisinty Sasmita Patra <sisinty.s.patra@intel.com>
 Copyright (c) 2015 Intel Corporation.
 
 This kit gives an insight, how to use various sensors that are available in starter kit.
 
-Linker Flags used:
-
--lupm-grove, -lupm-i2clcd, -lupm-buzzer, -lupm-mic, -lupm-ldt0028, -lupm-adxl345
+Linker flags:
+-lupm-grove -lupm-i2clcd -lupm-buzzer -lupm-mic -lupm-ldt0028 -lupm-mma7660
--- kits/starter/starterSample.cpp
+++ kits/starter/starterSample.cpp
@@ -1,7 +1,7 @@
 /*
  * Author: Sisinty Sasmita Patra <sisinty.s.patra@intel.com>
  * Copyright (c) 2015 Intel Corporation.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -9,7 +9,7 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
  *
@@ -17,20 +17,20 @@
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *   
+ *
  */
 
 #include <iostream>
 #include "jhd1313m1.h"   //lcd display
-#include "grove.h"     
-#include "adxl345.h"     //3-Axis digital accelerometer
-#include "buzzer.h"	 //buzzer
+#include "grove.h"
+#include "mma7660.h"     //3-Axis digital accelerometer 1.5g
+#include "buzzer.h"      //buzzer
 #include "mic.h"         //sound sensor
 #include "at42qt1070.h"  //Touch sensor
-#include "ldt0028.h"     //Piezo vibration sensor 
+#include "ldt0028.h"     //Piezo vibration sensor
 
 using namespace std;
 using namespace upm;
