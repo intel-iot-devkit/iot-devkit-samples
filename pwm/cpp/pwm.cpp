7ec03b419ee0348310dcebb6d6299dde5181a2f4
Joseph Butler <joseph.butler@intel.com>
2016-11-03 15:23:21 -0700

    Update pwm.cpp
    Signed-off-by: Joseph Butler <joseph.butler@intel.com>
    Signed-off-by: Thomas Ingleby <thomas.ingleby@intel.com>

--- pwm/cpp/pwm.cpp
+++ pwm/cpp/pwm.cpp
@@ -36,11 +36,12 @@
  */
 int main()
 {
-	// check that we are running on Galileo or Edison
+	// check that we are running on Galileo or Edison or Joule
 	mraa::Platform platform = mraa::getPlatformType();
 	if ((platform != mraa::INTEL_GALILEO_GEN1)
 	    && (platform != mraa::INTEL_GALILEO_GEN2)
-	    && (platform != mraa::INTEL_EDISON_FAB_C)) {
+	    && (platform != mraa::INTEL_EDISON_FAB_C)
+	    && (platform != mraa::INTEL_GT_TUCHUCK)) {
 	  std::cerr << "Unsupported platform, exiting" << std::endl;
 	  return mraa::ERROR_INVALID_PLATFORM;
 	}
