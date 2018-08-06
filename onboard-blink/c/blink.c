fa2e2c1f91f8feb46484320d90579df754bcab20
andrey4latyshev <andrey.latyshev@intel.com>
2018-08-06 18:48:32 +0300

    C-code of Blink sample handles error of write operation

--- onboard-blink/c/blink.c
+++ onboard-blink/c/blink.c
@@ -35,63 +35,69 @@
 
 int main()
 {
-	int gpioPin = 13;
-	char unknownPlatformMessage[] ="This sample uses the MRAA/UPM library for I/O access, "
-    		"you are running it on an unrecognized platform. "
-			"You may need to modify the MRAA/UPM initialization code to "
-			"ensure it works properly on your platform.\n\n";
+    int gpioPin = 13;
+    char unknownPlatformMessage[] ="This sample uses the MRAA/UPM library for I/O access, "
+            "you are running it on an unrecognized platform. "
+            "You may need to modify the MRAA/UPM initialization code to "
+            "ensure it works properly on your platform.\n\n";
 
-	// check which board we are running on
-	mraa_platform_t platform = mraa_get_platform_type();
-	switch (platform) {
-		case MRAA_UP2:
+    // check which board we are running on
+    mraa_platform_t platform = mraa_get_platform_type();
+    switch (platform) {
+        case MRAA_UP2:
 #ifdef USING_GROVE_PI_SHIELD
-			gpioPin = 4 + 512; // D4 Connector (512 offset needed for the shield)
-			break;
+            gpioPin = 4 + 512; // D4 Connector (512 offset needed for the shield)
+            break;
 #endif
-		case MRAA_UP:
-		case MRAA_INTEL_EDISON_FAB_C:
-		case MRAA_INTEL_GALILEO_GEN2:
-			break;
-		case MRAA_INTEL_MINNOWBOARD_MAX: // Same for Minnowboard Turbot
-			gpioPin = 104;
-			break;
-		case MRAA_INTEL_JOULE_EXPANSION:
-			gpioPin = 101;
-			break;
-		default:
-	        fprintf(stderr, "%s", unknownPlatformMessage);
-	}
+        case MRAA_UP:
+        case MRAA_INTEL_EDISON_FAB_C:
+        case MRAA_INTEL_GALILEO_GEN2:
+            break;
+        case MRAA_INTEL_MINNOWBOARD_MAX: // Same for Minnowboard Turbot
+            gpioPin = 104;
+            break;
+        case MRAA_INTEL_JOULE_EXPANSION:
+            gpioPin = 101;
+            break;
+        default:
+            fprintf(stderr, "%s", unknownPlatformMessage);
+    }
 #ifdef USING_GROVE_PI_SHIELD
-	mraa_add_subplatform(MRAA_GROVEPI, "0");
+    mraa_add_subplatform(MRAA_GROVEPI, "0");
 #endif
-	// check if running as root
-	int euid = geteuid();
-	if (euid) {
-		fprintf(stderr, "This project uses Mraa I/O operations, but you're not running as 'root'.\n"
-				"The IO operations below might fail.\n"
-				"See the project's Readme for more info.\n\n");
-	}
-	// create the pin object
-	mraa_gpio_context d_pin = mraa_gpio_init(gpioPin);
-	if (d_pin == NULL) {
-		fprintf(stderr, "MRAA couldn't initialize GPIO, exiting.\n");
-		return MRAA_ERROR_UNSPECIFIED;
-	}
+    // check if running as root
+    int euid = geteuid();
+    if (euid) {
+        fprintf(stderr, "This project uses Mraa I/O operations, but you're not running as 'root'.\n"
+                "The IO operations below might fail.\n"
+                "See the project's Readme for more info.\n\n");
+    }
+    // create the pin object
+    mraa_gpio_context d_pin = mraa_gpio_init(gpioPin);
+    if (d_pin == NULL) {
+        fprintf(stderr, "MRAA couldn't initialize GPIO, exiting.\n");
+        return MRAA_ERROR_UNSPECIFIED;
+    }
 
-	// set the pin as output
-	if (mraa_gpio_dir(d_pin, MRAA_GPIO_OUT) != MRAA_SUCCESS) {
-		fprintf(stderr, "Can't set digital pin as output, exiting.\n");
-		return MRAA_ERROR_UNSPECIFIED;
-	};
+    // set the pin as output
+    if (mraa_gpio_dir(d_pin, MRAA_GPIO_OUT) != MRAA_SUCCESS) {
+        fprintf(stderr, "Can't set digital pin as output, exiting.\n");
+        return MRAA_ERROR_UNSPECIFIED;
+    };
 
-	// loop forever toggling the on board LED every second
-	for (;;) {
-		mraa_gpio_write(d_pin, 0);
-		sleep(1);
-		mraa_gpio_write(d_pin, 1);
-		sleep(1);
-	}
+    // loop forever toggling the on board LED every second
+    for (;;) {
+        if (mraa_gpio_write(d_pin, 0) != MRAA_SUCCESS) {
+            fprintf(stderr, "MRAA cannot write pin value!\n");
+            return MRAA_ERROR_UNSPECIFIED;
+        }
+        sleep(1);
+        if (mraa_gpio_write(d_pin, 1) != MRAA_SUCCESS) {
+            fprintf(stderr, "MRAA cannot write pin value!\n");
+            return MRAA_ERROR_UNSPECIFIED;
+        }
+        sleep(1);
+    }
 
-	return MRAA_SUCCESS;
+    return MRAA_SUCCESS;
 }
